55dbc9010d84647398434cec7b50c8f2fb7865c3
Chin Yeung Li <tli@nexb.com>
2013-07-18 15:03:43 -0700

    Append the '.ABOUT' for files that are not ending with ".ABOUT"
    In addiition, I've added another required argument that need user to input the location for the generated about files

--- genabout.py
+++ genabout.py
@@ -21,11 +21,12 @@ file location, origin and license of the software components etc.
 """
 
 from __future__ import print_function
-from os.path import exists, dirname, join, abspath, isdir
 from os import makedirs
+from os.path import exists, dirname, join, abspath, isdir
 import csv
-import sys
+import errno
 import string
+import sys
 
 # TODO: version number
 __version__ = ''
@@ -45,19 +46,32 @@ def _exists(file_path):
 # the information into the .ABOUT file.
 # The current design is to assume the .ABOUT file doesn't exist and do nothing
 # if it does.
-def read_input_and_generate_output(input_file):
+def read_input_and_generate_output(input_file, gen_location):
     csvfile = csv.DictReader(open(input_file, 'rb'))
     for line in csvfile:
-        file_location = line['about_file']
-        dir = dirname(file_location)
+        try:
+            file_location = line['about_file']
+        except Exception, e:
+            print(repr(e))
+            sys.exit(errno.EINVAL)
+        location = join(gen_location, file_location)
+        about_file_location =''
+        if location.lower().endswith('.about'):
+            about_file_location = location
+        else:
+            if location[-1] == '/':
+                about_file_location = location.rpartition('/')[0] + '.ABOUT'
+            else:
+                about_file_location = location + '.ABOUT'
+        dir = dirname(about_file_location)
         if not _exists(dir):
             makedirs(dir)
-        if _exists(file_location):
+        if _exists(about_file_location):
             print("About file already existed.")
         else:
-            with open(file_location, 'wb') as output_file:
+            with open(about_file_location, 'wb') as output_file:
                 context = ""
-                for item in line:
+                for item in sorted(line.iterkeys()):
                     # The purpose of the replace('\n', '\n ') is used to
                     # format the continuation strings
                     value = line[item].replace('\n', '\n ')
@@ -66,20 +80,25 @@ def read_input_and_generate_output(input_file):
                 output_file.write(context)
 
 def main():
-    # The length is 2 as
+    # The length is 3 as
     # 1. the python script itself, genabout.py
     # 2. input file
-    if not len(sys.argv) == 2:
-        print(sys.argv[0] + " needs exactly 1 argument. \n\n \t genabout.py <input_file>")
+    # 3. location for generation
+    if not len(sys.argv) == 3:
+        print(sys.argv[0] + " needs exactly 2 arguments. \n\n \t genabout.py <input_file> <generated_location>")
         sys.exit(0)
 
     input_file = sys.argv[1]
+    gen_location = sys.argv[2]
 
     if not _exists(input_file):
         print(input_file, ': Input file does not exist.')
         sys.exit(0)
+    if not _exists(gen_location):
+        print(gen_location, ': Generated location does not exist.')
+        sys.exit(0)
 
-    read_input_and_generate_output(input_file)
+    read_input_and_generate_output(input_file, gen_location)
 
 if __name__ == "__main__":
     main()
