3c66215b3cef48c58c2afaa922a913925cbf9225
Chin Yeung Li <tli@nexb.com>
2013-09-18 16:09:17 -0700

    Fixed #38
    Created test code for genabout.py
    
    Created the gen_tests.py for this stage which will be integrate with the main test later.
    
    In addition, this commit make the genabout.py become a class and code enhancement have been made.

---
+++ gen_tests.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# =============================================================================
+#  Copyright (c) 2013 by nexB, Inc. http://www.nexb.com/ - All rights reserved.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# =============================================================================
+
+from __future__ import with_statement
+
+import os
+import shutil
+import string
+from StringIO import StringIO
+import tempfile
+import unittest
+
+import genabout
+
+class GenAboutTest(unittest.TestCase):
+    def test_read_input(self):
+        gen = genabout.GenAbout()
+        test_input = "testdata/test_files_for_genabout/about.csv"
+        list = gen.read_input(test_input)
+        self.assertTrue(list, "List shouldn't be empty.")
+
+    def test_read_input_missing_about_file(self):
+        gen = genabout.GenAbout()
+        test_input = "testdata/test_files_for_genabout/missing_about_file.csv"
+        list = gen.read_input(test_input)
+        self.assertTrue(len(gen.errors) == 1, "This should return only 1 error.")
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertFalse(list, "The list should be empty.")
+
+    def test_read_input_missing_about_resource(self):
+        gen = genabout.GenAbout()
+        test_input = "testdata/test_files_for_genabout/missing_about_resource.csv"
+        list = gen.read_input(test_input)
+        self.assertTrue(len(gen.errors) == 1, "This should return only 1 error.")
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertFalse(list, "The list should be empty.")
+
+    def test_read_input_missing_about_file_and_resource(self):
+        gen = genabout.GenAbout()
+        test_input = "testdata/test_files_for_genabout/missing_about_file_and_resource.csv"
+        list = gen.read_input(test_input)
+        self.assertTrue(len(gen.errors) == 1, "This should return only 1 error.")
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertFalse(list, "The list should be empty.")
+
+    def test_read_input_valid_and_invalid_rows(self):
+        gen = genabout.GenAbout()
+        test_input = "testdata/test_files_for_genabout/valid_and_invalid_rows.csv"
+        list = gen.read_input(test_input)
+        self.assertTrue(len(gen.errors) == 2, "This should return 2 errors.")
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertTrue(len(list) == 1, "The length of the list should be 1.")
+
+    def test_pre_generation_about_exists_action_0(self):
+        gen = genabout.GenAbout()
+        gen_location = "testdata/test_files_for_genabout/"
+        action_num = '0'
+        input_list = [[{'about_file': 'about.py.ABOUT', 'version': '0.8.1',
+                        'about_resource': '.', 'name': 'ABOUT tool'}]]
+        expected_output_list = []
+        output_list = gen.pre_generation(gen_location, input_list, action_num)
+        self.assertTrue(expected_output_list == output_list, "This output_list should be empty.")
+        self.assertTrue(len(gen.warnings) == 1, "Should return 1 warnings.")
+        self.assertFalse(gen.errors, "No errors should be returned.")
+
+    def test_pre_generation_about_exists_action_1(self):
+        gen = genabout.GenAbout()
+        gen_location = "testdata/test_files_for_genabout/"
+        action_num = '1'
+        input_list = [[{'about_file': 'about.py.ABOUT', 'version': '0.8.2',
+                        'about_resource': '.', 'name': ''}]]
+        expected_output_list = [['testdata/test_files_for_genabout/about.py.ABOUT',
+                                 {'about_file': 'about.py.ABOUT', 'version': '0.8.2',
+                                  'about_resource': '.', 'name': 'ABOUT tool'}]]
+        output_list = gen.pre_generation(gen_location, input_list, action_num)
+        self.assertTrue(expected_output_list == output_list)
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertFalse(gen.errors, "No errors should be returned.")
+
+    def test_pre_generation_about_exists_action_2(self):
+        gen = genabout.GenAbout()
+        gen_location = "testdata/test_files_for_genabout/"
+        action_num = '2'
+        input_list = [[{'about_file': 'about.py.ABOUT', 'version': '0.8.2',
+                        'about_resource': '.', 'name': '', 'test': 'test sample'}]]
+        expected_output_list = [['testdata/test_files_for_genabout/about.py.ABOUT',
+                                  {'test': 'test sample', 'about_file': 'about.py.ABOUT',
+                                    'version': '0.8.1', 'about_resource': '.',
+                                     'name': 'ABOUT tool'}]]
+        output_list = gen.pre_generation(gen_location, input_list, action_num)
+        self.assertTrue(expected_output_list == output_list)
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertFalse(gen.errors, "No errors should be returned.")
+
+    def test_pre_generation_about_exists_action_3(self):
+        gen = genabout.GenAbout()
+        gen_location = "testdata/test_files_for_genabout/"
+        action_num = '3'
+        input_list = [[{'about_file': 'about.py.ABOUT', 'version': '0.8.2',
+                        'about_resource': '.', 'name': '', 'test': 'test sample'}]]
+        expected_output_list = [['testdata/test_files_for_genabout/about.py.ABOUT',
+                                  {'about_file': 'about.py.ABOUT', 'version': '0.8.2',
+                                    'about_resource': '.', 'name': '', 'test': 'test sample'}]]
+        output_list = gen.pre_generation(gen_location, input_list, action_num)
+        self.assertTrue(expected_output_list == output_list)
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertFalse(gen.errors, "No errors should be returned.")
+
+    def test_format_output(self):
+        gen = genabout.GenAbout()
+        input_list = [['testdata/test_files_for_genabout/about.py.ABOUT',
+                        {'about_file': 'about.py.ABOUT', 'version': '0.8.1',
+                          'about_resource': '.', 'name': 'ABOUT Tool'}]]
+        expected_output = [['testdata/test_files_for_genabout/about.py.ABOUT',
+                             'about_resource: .\nname: ABOUT Tool\nversion: 0.8.1\n\nabout_file: about.py.ABOUT\n']]
+        output = gen.format_output(input_list)
+        self.assertTrue(expected_output == output)
+        self.assertFalse(gen.warnings, "No warnings should be returned.")
+        self.assertFalse(gen.errors, "No errors should be returned.")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- genabout.py
+++ genabout.py
@@ -32,7 +32,6 @@ import os
 import sys
 
 
-# TODO: version number
 __version__ = '0.8.1'
 
 
@@ -42,151 +41,150 @@ SKIPPED_FIELDS = ['warnings', 'errors']
 Warn = namedtuple('Warn', 'field_name message',)
 Error = namedtuple('Error', 'field_name message',)
 
+class GenAbout(object):
+    
+    def __init__(self):
+        self.warnings = []
+        self.errors = []
 
-errors = []
-warnings = []
+    def read_input(self, input_file):
+        csvfile = csv.DictReader(open(input_file, 'rb'))
+        components_list = []
+        for line in csvfile:
+            file_list = []
+            if not line['about_file']:
+                missing_about_file = "'about_file' field value is missing. Generation is skipped."
+                self.errors.append(Error(None, missing_about_file))
+                continue
+            if not line['about_resource']:
+                missing_about_resource = "'about_resource' is missing. Generation is skipped."
+                self.errors.append(Error(line['about_file'], missing_about_resource))
+                continue
+            file_list.append(line)
+            components_list.append(file_list)
+        return components_list
+
+
+    def pre_generation(self, gen_location, input_list, action_num):
+        """
+        check the existence of the output location and handle differently
+        according to the action_num.
+        """
+        output_list = []
+        for component in input_list:
+            for line in component:
+                component_list = []
+                file_location = line['about_file']
+                if file_location.startswith('/'):
+                    file_location = file_location.partition('/')[2]
+                about_file_location = join(gen_location, file_location)
+                dir = dirname(about_file_location)
+                if not _exists(dir):
+                    makedirs(dir)
+                if _exists(about_file_location):
+                    if action_num == '0':
+                        about_exist = "ABOUT file already existed. Generation is skipped."
+                        self.warnings.append(Warn(about_file_location, about_exist))
+                        continue
+                    # Overwrites the current ABOUT field value if existed
+                    elif action_num == '1':
+                        about_object = about.AboutFile(about_file_location)
+                        for field_name, value in about_object.parsed.items():
+                            field_name = field_name.lower()
+                            if not field_name in line.keys() or not line[field_name]:
+                                line[field_name] = value
+                    # Keep the current field value and only add the "new" field and field value
+                    elif action_num == '2':
+                        about_object = about.AboutFile(about_file_location)
+                        for field_name, value in about_object.parsed.items():
+                            field_name = field_name.lower()
+                            line[field_name] = value
+                    # We don't need to do anything for the action_num = 3 as
+                    # the original ABOUT file will be replaced in the write_output()
+                component_list.append(about_file_location)
+                component_list.append(line)
+                output_list.append(component_list)
+        return output_list
+
+
+    def format_output(self, input_list):
+        """
+        process the input and covert to the specific strings format
+        """
+        components_list = []
+        for items in input_list:
+            component = []
+            about_file_location = items[0]
+            about_dict_list = items[1]
+            context = ''
+            if about_dict_list['name']:
+                name = about_dict_list['name']
+            else:
+                name = ''
+            if about_dict_list['version']:
+                version = about_dict_list['version']
+            else:
+                version = ''
+            context = 'about_resource: ' + about_dict_list['about_resource'] + '\n' \
+                        + 'name: ' + name + '\n' \
+                        + 'version: ' + version + '\n\n'
+            for item in sorted(about_dict_list.iterkeys()):
+                if not item in MANDATORY_FIELDS:
+                    # The purpose of the replace('\n', '\n ') is used to
+                    # format the continuation strings
+                    value = about_dict_list[item].replace('\n', '\n ')
+                    if (value or item in MANDATORY_FIELDS) and not item in SKIPPED_FIELDS:
+                        context += item + ': ' + value + '\n'
+            component.append(about_file_location)
+            component.append(context)
+            components_list.append(component)
+        return components_list
+
+
+    def write_output(self, output):
+        for line in output:
+            about_file_location = line[0]
+            context = line[1]
+            os.remove(about_file_location)
+            with open(about_file_location, 'wb') as output_file:
+                output_file.write(context)
+
+
+    def warnings_errors_summary(self, gen_location, show_error_num):
+        display_error = False
+        display_warning = False
+        if show_error_num == '1':
+            display_error = True
+        if show_error_num == '2':
+            display_error = True
+            display_warning = True
+        if self.errors or self.warnings:
+            error_location = gen_location + 'error.txt' if gen_location.endswith('/') else gen_location + '/error.txt'
+            errors_num = len(self.errors)
+            warnings_num = len(self.warnings)
+            if _exists(error_location):
+                print("error.txt existed and will be replaced.")
+            with open(error_location, 'wb') as error_file:
+                if self.warnings:
+                    for warning_msg in self.warnings:
+                        if display_warning:
+                            print(str(warning_msg))
+                        error_file.write(str(warning_msg) + '\n')
+                if self.errors:
+                    for error_msg in self.errors:
+                        if display_error:
+                            print(str(error_msg))
+                        error_file.write(str(error_msg) + '\n')
+                error_file.write('\n' + 'Warnings: %s' % warnings_num)
+                error_file.write('\n' + 'Errors: %s' % errors_num)
+            print('Warnings: %s' % warnings_num)
+            print('Errors: %s' % errors_num)
+            print("See %s for the error/warning log." % error_location)
 
 
 def _exists(file_path):
     if file_path:
         return exists(abspath(file_path))
-
-
-def read_input(input_file):
-    csvfile = csv.DictReader(open(input_file, 'rb'))
-    components_list = []
-    for line in csvfile:
-        file_list = []
-        if not line['about_file']:
-            missing_about_file = "'about_file' field value is missing. Generation is skipped."
-            errors.append(Error(None, missing_about_file))
-            continue
-        if not line['about_resource']:
-            missing_about_resource = "'about_resource' is missing. Generation is skipped."
-            errors.append(Error(line['about_file'], missing_about_resource))
-            continue
-        file_list.append(line)
-        components_list.append(file_list)
-    return components_list
-
-
-def pre_generation(gen_location, input_list, action_num):
-    """
-    check the existence of the output location and handle differently
-    according to the action_num.
-    """
-    output_list = []
-    for component in input_list:
-        for line in component:
-            component_list = []
-            file_location = line['about_file']
-            if file_location.startswith('/'):
-                file_location = file_location.partition('/')[2]
-            about_file_location = join(gen_location, file_location)
-            dir = dirname(about_file_location)
-            if not _exists(dir):
-                makedirs(dir)
-            if _exists(about_file_location):
-                if action_num == '0':
-                    about_exist = "ABOUT file already existed. Generation is skipped."
-                    warnings.append(Warn(about_file_location, about_exist))
-                    continue
-                # Overwrites the current ABOUT field value if existed
-                elif action_num == '1':
-                    about_object = about.AboutFile(about_file_location)
-                    for field_name, value in about_object.parsed.items():
-                        field_name = field_name.lower()
-                        if not field_name in line.keys() or not line[field_name]:
-                            line[field_name] = value
-                    os.remove(about_file_location)
-                # Keep the current field value and only add the "new" field and field value
-                elif action_num == '2':
-                    about_object = about.AboutFile(about_file_location)
-                    for field_name, value in about_object.parsed.items():
-                        field_name = field_name.lower()
-                        line[field_name] = value
-                    os.remove(about_file_location)
-                elif action_num == '3':
-                    os.remove(about_file_location)
-            component_list.append(about_file_location)
-            component_list.append(line)
-            output_list.append(component_list)
-    return output_list
-
-
-def format_output(input_list):
-    """
-    process the input and covert to the specific strings format
-    """
-    components_list = []
-    for items in input_list:
-        component = []
-        about_file_location = items[0]
-        about_dict_list = items[1]
-
-        context = ''
-        if about_dict_list['name']:
-            name = about_dict_list['name']
-        else:
-            name = ''
-        if about_dict_list['version']:
-            version = about_dict_list['version']
-        else:
-            version = ''
-        context = 'about_resource: ' + about_dict_list['about_resource'] + '\n' \
-                    + 'name: ' + name + '\n' \
-                    + 'version: ' + version + '\n\n'
-        for item in sorted(about_dict_list.iterkeys()):
-            if not item in MANDATORY_FIELDS:
-                # The purpose of the replace('\n', '\n ') is used to
-                # format the continuation strings
-                value = about_dict_list[item].replace('\n', '\n ')
-                if (value or item in MANDATORY_FIELDS) and not item in SKIPPED_FIELDS:
-                    context += item + ': ' + value + '\n'
-        component.append(about_file_location)
-        component.append(context)
-        components_list.append(component)
-    return components_list
-
-
-def write_output(output):
-    for line in output:
-        about_file_location = line[0]
-        context = line[1]
-        with open(about_file_location, 'wb') as output_file:
-            output_file.write(context)
-
-
-def warnings_errors_summary(gen_location, show_error_num):
-    display_error = False
-    display_warning = False
-    if show_error_num == '1':
-        display_error = True
-    if show_error_num == '2':
-        display_error = True
-        display_warning = True
-    if errors or warnings:
-        error_location = gen_location + 'error.txt' if gen_location.endswith('/') else gen_location + '/error.txt'
-        errors_num = len(errors)
-        warnings_num = len(warnings)
-        if _exists(error_location):
-            print("error.txt existed and will be replaced.")
-        with open(error_location, 'wb') as error_file:
-            if warnings:
-                for warning_msg in warnings:
-                    if display_warning:
-                        print(str(warning_msg))
-                    error_file.write(str(warning_msg) + '\n')
-            if errors:
-                for error_msg in errors:
-                    if display_error:
-                        print(str(error_msg))
-                    error_file.write(str(error_msg) + '\n')
-            error_file.write('\n' + 'Warnings: %s' % warnings_num)
-            error_file.write('\n' + 'Errors: %s' % errors_num)
-        print('Warnings: %s' % warnings_num)
-        print('Errors: %s' % errors_num)
-        print("See %s for the error/warning log." % error_location)
 
 
 def syntax():
@@ -289,11 +287,12 @@ def main(args, opts):
         print(gen_location, ': Generated location does not exist.')
         sys.exit(errno.EIO)
 
-    input_list = read_input(input_file)
-    components_list = pre_generation(gen_location, input_list, opt_arg_num)
-    formatted_output = format_output(components_list)
-    write_output(formatted_output)
-    warnings_errors_summary(gen_location, verb_arg_num)
+    gen = GenAbout()
+    input_list = gen.read_input(input_file)
+    components_list = gen.pre_generation(gen_location, input_list, opt_arg_num)
+    formatted_output = gen.format_output(components_list)
+    gen.write_output(formatted_output)
+    gen.warnings_errors_summary(gen_location, verb_arg_num)
 
 if __name__ == "__main__":
     longopts = ['help', 'version', 'action=', 'verbosity=']
