93a2a5f4e297e4494d7be61f0096fffc24dc68ef
Chin Yeung Li <tli@nexb.com>
2014-04-16 16:05:30 -0700

    Implement the optparser to the genattrib

--- about_code_tool/genattrib.py
+++ about_code_tool/genattrib.py
@@ -30,21 +30,48 @@ import errno
 import fnmatch
 import getopt
 import httplib
+import logging
+import optparse
 import posixpath
 import socket
 import string
 import sys
 import urlparse
+
 from collections import namedtuple
 from datetime import datetime
 from email.parser import HeaderParser
 from os import listdir, walk
 from os.path import exists, dirname, join, abspath, isdir, basename, normpath
 from StringIO import StringIO
+
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setLevel(logging.CRITICAL)
+handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+logger.addHandler(handler)
+
+
 __version__ = '0.9.0'
 
-# see http://dejacode.org
-__about_spec_version__ = '0.8.0'
+__about_spec_version__ = '0.8.0'  # See http://dejacode.org
+
+__copyright__ = """
+Copyright (c) 2013-2014 nexB Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 def component_subset_to_sublist(input_path):
     sublist = []
@@ -55,86 +82,39 @@ def component_subset_to_sublist(input_path):
 
     return sublist
 
-def syntax():
-    print("""
-Syntax:
-    genattrib.py [Options] [Input] [Output] [Component List]
+USAGE_SYNTAX = """\
     Input can be a file or directory.
     Output of rendered template must be a file (e.g. .html).
     Component List must be a .csv file which has at least an "about_resource" column.
-""")
+"""
+
+VERBOSITY_HELP = """\
+Print more or fewer verbose messages while processing ABOUT files
+0 - Do not print any warning or error messages, just a total count (default)
+1 - Print error messages
+2 - Print error and warning messages
+"""
 
 
-def option_usage():
-    print("""
-Options:
-    --overwrite          Overwrites the output file if it exists
-    -v,--version         Display current version, license notice, and copyright notice
-    -h,--help            Display help
-    --verbosity  <arg>   Print more or less verbose messages while processing ABOUT files
-        <arg>
-            0 - Do not print any warning or error messages, just a total count (default)
-            1 - Print error messages
-            2 - Print error and warning messages
-""")
+def main(parser, options, args):
+    overwrite = options.overwrite
+    verbosity = options.verbosity
+    
+    if options.version:
+        print('ABOUT tool {0}\n{1}'.format(__version__, __copyright__))
+        sys.exit(0)
 
+    if verbosity == 1:
+        handler.setLevel(logging.ERROR)
+    elif verbosity >= 2:
+        handler.setLevel(logging.WARNING)
 
-def version():
-    print("""
-ABOUT CODE: Version: %s
-Copyright (c) 2013 nexB Inc. All rights reserved.
-http://dejacode.org
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations
-under the License.""" % __version__)
+    if not len(args) == 3:
+        print('Path for input, output and component list are required.\n')
+        parser.print_help()
+        sys.exit(errno.EEXIST)
 
-
-def main(args, opts):
-    overwrite = False
-    opt_arg_num = '0'
-    for opt, opt_arg in opts:
-        invalid_opt = True
-        if opt in ('-h', '--help'):
-            syntax()
-            option_usage()
-            sys.exit(0)
-
-        if opt in ('-v', '--version'):
-            version()
-            sys.exit(0)
-
-        if opt in ('--verbosity'):
-            invalid_opt = False
-            valid_opt_args = ['0', '1', '2']
-            if not opt_arg or not opt_arg in valid_opt_args:
-                print("Invalid option argument.")
-                option_usage()
-                sys.exit(errno.EINVAL)
-            else:
-                opt_arg_num = opt_arg
-
-        if opt in ('--overwrite'):
-            invalid_opt = False
-            overwrite = True
-
-        if invalid_opt:
-            assert False, 'Unsupported option.'
-
-    if len(args) < 2:
-        print('Input and output parameters are mandatory.')
-        syntax()
-        option_usage()
-        sys.exit(errno.EINVAL)
-
-    input_path = args[0]
-    output_path = args[1]
-    component_subset_path = None if len(args) < 3 else args[2]
+    input_path, output_path, component_subset_path = args
 
     # TODO: need more path normalization (normpath, expanduser)
     # input_path = abspath(input_path)
@@ -147,23 +127,23 @@ def main(args, opts):
 
     if not exists(input_path):
         print('Input path does not exist.')
-        option_usage()
+        parser.print_help()
         sys.exit(errno.EEXIST)
 
     if isdir(output_path):
         print('Output must be a file, not a directory.')
-        option_usage()
+        parser.print_help()
         sys.exit(errno.EISDIR)
 
     if exists(output_path) and not overwrite:
         print('Output file already exists. Select a different file name or use '
               'the --overwrite option.')
-        option_usage()
+        parser.print_help()
         sys.exit(errno.EEXIST)
 
     if component_subset_path and not exists(component_subset_path):
         print('Component Subset path does not exist.')
-        option_usage()
+        parser.print_help()
         sys.exit(errno.EEXIST)
 
     if not exists(output_path) or (exists(output_path) and overwrite):
@@ -178,14 +158,56 @@ def main(args, opts):
         assert False, "Unsupported option(s)."
 
 
-if __name__ == "__main__":
-    longopts = ['help', 'version', 'overwrite', 'verbosity=']
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hv', longopts)
-    except Exception as e:
-        print(repr(e))
-        syntax()
-        option_usage()
-        sys.exit(errno.EINVAL)
+def get_parser():
+    class MyFormatter(optparse.IndentedHelpFormatter):
+        def _format_text(self, text):
+            """
+            Overridden to allow description to be printed without
+            modification
+            """
+            return text
 
-    main(args, opts)
+        def format_option(self, option):
+            """
+            Overridden to allow options help text to be printed without
+            modification
+            """
+            result = []
+            opts = self.option_strings[option]
+            opt_width = self.help_position - self.current_indent - 2
+            if len(opts) > opt_width:
+                opts = "%*s%s\n" % (self.current_indent, "", opts)
+                indent_first = self.help_position
+            else:                       # start help on same line as opts
+                opts = "%*s%-*s  " % (self.current_indent, "", opt_width, opts)
+                indent_first = 0
+            result.append(opts)
+            if option.help:
+                help_text = self.expand_default(option)
+                help_lines = help_text.split('\n')
+                result.append("%*s%s\n" % (indent_first, "", help_lines[0]))
+                result.extend(["%*s%s\n" % (self.help_position, "", line)
+                               for line in help_lines[1:]])
+            elif opts[-1] != "\n":
+                result.append("\n")
+            return "".join(result)
+
+    parser = optparse.OptionParser(
+        usage='%prog [options] input_path output_path component_list',
+        description=USAGE_SYNTAX,
+        add_help_option=False,
+        formatter=MyFormatter(),
+    )
+    parser.add_option("-h", "--help", action="help", help="Display help")
+    parser.add_option("-v", "--version", action="store_true",
+        help='Display current version, license notice, and copyright notice')
+    parser.add_option('--overwrite', action='store_true',
+                      help='Overwrites the output file if it exists')
+    parser.add_option('--verbosity', type=int, help=VERBOSITY_HELP)
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    options, args = parser.parse_args()
+    main(parser, options, args)
