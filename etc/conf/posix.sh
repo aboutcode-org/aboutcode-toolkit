b1dda6827c8fdbeb001bf87e575295a55fd5a35d
Philippe Ombredanne <pombredanne@nexb.com>
2014-09-02 17:15:35 +0200

    Fixing comment, mistakenly made as a dos comment

--- etc/conf/posix.sh
+++ etc/conf/posix.sh
@@ -1,2 +1,2 @@
-@rem work around heisenbugs in https://bitbucket.org/hpk42/pytest/issue/576/random-test-failures-on-windows-after
+# work around heisenbugs in https://bitbucket.org/hpk42/pytest/issue/576/random-test-failures-on-windows-after
 set PYTHONDONTWRITEBYTECODE=1
