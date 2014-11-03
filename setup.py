866f7511ae4a2136b81526dcb6b6c9b644def5d0
Philippe Ombredanne <pombredanne@nexb.com>
2014-11-03 12:28:52 +0100

    Working setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,12 @@ setup(
     description=('Document the provenance (origin and license) of '
                 'third-party software using small text files. '
                 'Collect inventories, generate attribution documentation.'),
+
     author=('Jillian Daguil, Thomas Druez, Chin-Yeung Li, '
             'Philippe Ombredanne and others.'),
+
     author_email='info@nexb.com',
+
     url='http://dejacode.org',
 
     long_description=('AboutCode provides a simple way to document the'
@@ -24,39 +27,33 @@ setup(
         'you use in your project. This documentation is stored in *.ABOUT'
         'files, side-by-side with the documented code.'),
 
-    license='Apache License 2.0',
+    license='Apache 2.0',
     packages=find_packages(),
-    package_data={
-        'about_code_tool': ['templates/*'],
-        'about_code_tool': ['tests/testdata/*'],
-    },
     include_package_data=True,
-    zip_safe=False,
 
+    data_files=[('about', ['about.ABOUT',
+#                       'about.bat',
+#                       'about',
+                      'about.cfg',
+                      'README.rst',
+                      'apache-2.0.LICENSE',
+                      'NOTICE',
+                      'SPEC',
+                      'USAGE',
+                      ]), ],
+
+    zip_safe=False,
     entry_points='''
         [console_scripts]
         about-code=about_code_tool.cmd:cli
     ''',
-#     install_requires=[
-#         'Jinja2',
-#         'MarkuSafe',
-#         'click',
-#         'jinja2==2.7.3'
-#         'MarkupSafe==0.23'
-#         'schematics==0.9-5'
-#         'unicodecsv==0.9.4'
-#         'pytest==2.6.1'
-#         'colorama==0.3.1'
-#         'py==1.4.23'
-#     ],
+    install_requires=[
+        'jinja2 >= 2.7.3',
+        'click >= 3.2',
+        'unicodecsv >= 0.9.4',
+     ],
 
-     test_suite='about_code_tool.tests',
-#     tests_require=[
-#         'pytest',
-#         'py',
-#         'colorama',
-#     ],
-
+    test_suite='about_code_tool.tests',
     platforms='any',
     classifiers=[
         'Development Status :: 4 - Beta',
