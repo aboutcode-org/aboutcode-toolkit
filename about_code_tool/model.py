3bb44cc77691d65f8a0f7ff0d58db909c78ff724
Chin Yeung Li <tli@nexb.com>
2015-05-19 10:17:17 -0700

    Fix the test for "test_loads_dumps_is_idempotent". The function "serialize" is used for both reading input csv and ABOUT files. We need to update the code to properly read the YAML's multi-line format for ABOUT files.

--- about_code_tool/model.py
+++ about_code_tool/model.py
@@ -138,11 +138,17 @@ class Field(object):
             value = value.splitlines(True)
             # multi-line
             if len(value) > 1:
-                # Insert '|' as the indicator for multi-line follow by a
-                # newline character
-                value.insert(0, u'|\n')
-                # insert 4 spaces for newline values
-                value = u'    '.join(value)
+                # This code is used to read the YAML's multi-line format in
+                # ABOUT files
+                # (Test: test_loads_dumps_is_idempotent)
+                if value[0].strip() == u'|' or value[0].strip() == u'>':
+                    value = u' '.join(value)
+                else:
+                    # Insert '|' as the indicator for multi-line follow by a
+                    # newline character
+                    value.insert(0, u'|\n')
+                    # insert 4 spaces for newline values
+                    value = u'    '.join(value)
             else:
                 value = u''.join(value)
             #print(value)
