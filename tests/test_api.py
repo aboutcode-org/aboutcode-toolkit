77c33a4c449c3249fb097aa98706dc066ba5ac7f
tdruez <thomas.druez@gmail.com>
2017-11-15 11:31:05 +0800

    Fix Python2/Python3 compatibility for the api module #280
    - Fix urllib related imports
    - Remove all the dead code
    - Code cleanup
    
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -22,30 +22,20 @@ import unittest
 
 from mock import patch
 
-import attributecode
 from attributecode import api
-from attributecode.api import LicenseInfo
 
 
 class ApiTest(unittest.TestCase):
-
-    def test_build_api_url(self):
-        url = 'http:/dejacode.org/'
-        api_username = 'phi'
-        api_key = 'ABCD'
-        license_key = 'apache'
-        expected = 'http:/dejacode.org/apache/?username=phi&api_key=ABCD&format=json'
-        result = api.build_api_url(url, api_username, api_key, license_key)
-        assert expected == result
-
-    @patch.object(attributecode.api, 'get_license_data')
-    def test_get_license_info(self, mock_data):
-        mock_data.return_value = [], {'key': 'test', 'name': 'test_name', 'full_text': 'test_full_text' }
-        result = api.get_license_info(self, '', '', '', '')
-        assert result == ([], LicenseInfo(key='test', name='test_name', text='test_full_text'))
-
-    @patch.object(attributecode.api, 'request_license_data')
+    @patch.object(api, 'request_license_data')
     def test_get_license_details_from_api(self, mock_data):
-        mock_data.return_value = {'name': 'test_name', 'full_text': 'test_full_text', 'key': 'test'}, []
-        result = api.get_license_details_from_api('', '', '')
-        assert result == ('test_name', 'test', 'test_full_text', [])
+        license_data = {
+            'name': 'Apache License 2.0',
+            'full_text': 'Apache License Version 2.0 ...',
+            'key': 'apache-2.0',
+        }
+        errors = []
+        mock_data.return_value = license_data, errors
+
+        expected = ('Apache License 2.0', 'apache-2.0', 'Apache License Version 2.0 ...', [])
+        result = api.get_license_details_from_api('url', 'api_key', 'license_key')
+        assert expected == result
