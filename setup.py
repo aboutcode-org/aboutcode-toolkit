1e970f70fb77b55b97a79c491ce75c6182361796
Chin Yeung Li <tli@nexb.com>
2018-10-23 06:26:01 +0800

    Bump version to 3.2.0 in setup.py
    Signed-off-by: Chin Yeung Li <tli@nexb.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='aboutcode-toolkit',
-    version='3.1.3',
+    version='3.2.0',
     license='Apache-2.0',
     description=(
         'AboutCode-toolkit is a tool to document the provenance (origin and license) of '
