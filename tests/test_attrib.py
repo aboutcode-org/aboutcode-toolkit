b7e1bbda69d5e5cfe2e6b4a6405a667013dc3912
Chin Yeung Li <tli@nexb.com>
2019-06-17 08:02:03 +0800

    strip whitespace when doing comparison

--- tests/test_attrib.py
+++ tests/test_attrib.py
@@ -118,8 +118,8 @@ class GenerateTest(unittest.TestCase):
         errors, abouts = model.collect_inventory(test_file)
         attrib.generate_and_save(abouts, output_file, template_loc)
 
-        f1 = open(output_file, "r").read()
-        f2 = open(expected,"r").read()
+        f1 = open(output_file, "r").read().strip()
+        f2 = open(expected,"r").read().strip()
         assert f1 == f2
 
 def remove_timestamp(html_text):
