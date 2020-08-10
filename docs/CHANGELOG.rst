0ecf774fe5271a4bac284ef4612ac302226b4cfc
Chin Yeung Li <tli@nexb.com>
2020-08-10 17:10:19 +0800

    Fixed #436
     * Fixed the copy file issue caused by multi-line in file fields

--- docs/CHANGELOG.rst
+++ docs/CHANGELOG.rst
@@ -5,7 +5,8 @@
     * Update transform code (See #427 and #428)
     * Fixed #431 - Error handling for empty "_file" fields
     * Fixed #432 - Handled UTF-8 variant invented by Microsoft
-    * Fixed #433 - problem was caused by the different multi-lic file format  between json and CSV (CSV with '\n' line break)
+    * Fixed #433 - problem was caused by the different multi-lic file format between json and CSV (CSV with '\n' line break)
+    * Fixed #436 - issue about copy with the `--reference` option
 
 2020-05-05
     Release 4.0.2
--- tests/test_util.py
+++ tests/test_util.py
@@ -26,6 +26,7 @@ import saneyaml
 
 from testing_utils import extract_test_loc
 from testing_utils import get_test_loc
+from testing_utils import get_temp_dir
 from testing_utils import on_posix
 from testing_utils import on_windows
 
@@ -611,3 +612,19 @@ description: sample
         abouts = [a, b]
         results = util.unique(abouts)
         assert [a] == results
+
+    def test_copy_license_notice_files(self):
+        base_dir = get_temp_dir()
+        reference_dir = get_test_loc('test_util/licenses')
+        fields = [(u'license_expression', u'mit or public-domain'),
+                  (u'about_resource', u'.'),
+                  (u'name', u'test'),
+                  (u'license_key', [u'mit', u'public-domain']),
+                  (u'license_file', [u'mit.LICENSE, mit2.LICENSE', u'public-domain.LICENSE'])]
+        util.copy_license_notice_files(fields, base_dir, reference_dir, '')
+        licenses = ['mit.LICENSE', 'mit2.LICENSE', 'public-domain.LICENSE']
+        from os import listdir
+        copied_files = listdir(base_dir)
+        assert len(licenses) == len(copied_files)
+        for license in licenses:
+            assert license in copied_files
