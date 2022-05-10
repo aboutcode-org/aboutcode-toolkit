fcf46336962d61a4e16a7b9d7e9eb6e1a29163b9
Philippe Ombredanne <pombredanne@nexb.com>
2022-05-10 15:52:11 +0200

    Sync with SCTK settings
    Also streamline test dependencies, remove mock for unittest.mock
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,9 +13,9 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ============================================================================
-import unittest
 
-import mock
+import unittest
+from unittest import mock
 
 from attributecode import api
 from attributecode import ERROR
